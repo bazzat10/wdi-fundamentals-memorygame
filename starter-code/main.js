0f164e709d928e30fe3134d9ca901f20d35337a7
Barry Tabios <Barry@Barrys-MacBook-Pro.local>
2017-01-04 00:38:34 +1100

    created cards with javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,11 +4,36 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-if (cardTwo == cardThree)
+/*if (cardTwo == cardThree)
 {
 	alert('You have found a match!');
+};
 }
 else
 {
 	alert('Sorry, try again')
-}
+}*/
+
+ var boardGame = document.getElementById("game-board");
+//var i;
+
+var createCards = function()
+{
+
+	for (var i=0; i<4; i++)
+	{
+		console.log('hello');
+
+	   var cards = document.createElement("div");
+	   cards.className = "card";
+	   boardGame.appendChild(cards);
+
+	 
+	}
+
+
+};
+
+
+
+
