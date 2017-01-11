7a2b25161884fb3ab6d8867f7caa6f47e96c9b58
Barry Tabios <Barry@Barrys-MacBook-Pro.local>
2017-01-11 20:46:31 +1100

    auto generate cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,34 +6,31 @@ var cardFour = "queen";
 
 /*if (cardTwo == cardThree)
 {
-	alert('You have found a match!');
+    alert('You have found a match!');
 };
 }
 else
 {
-	alert('Sorry, try again')
+    alert('Sorry, try again')
 }*/
-
- var boardGame = document.getElementById("game-board");
 //var i;
 
-var createCards = function()
+var createCards = function(x)
 {
 
-	for (var i=0; i<4; i++)
-	{
-		console.log('hello');
+	var boardGame = document.getElementById('game-board');
 
-	   var cards = document.createElement("div");
-	   cards.className = "card";
-	   boardGame.appendChild(cards);
-
-	 
-	}
+    for (var i=0; i<x; i++)
+    {
+   
+       var cards = document.createElement("div");
+       cards.className = "card";
+       boardGame.appendChild(cards);
+    
+    };
 
 
 };
-
 
 
 
